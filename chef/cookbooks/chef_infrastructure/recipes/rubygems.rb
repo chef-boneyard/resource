0a6551dbb4b460c8b1cb1ef5b4409fcdf943512b
John Keiser <john@johnkeiser.com>
2015-02-13 14:24:46 -0800

    Be rid of debugging stuff

--- chef/cookbooks/chef_infrastructure/recipes/rubygems.rb
+++ chef/cookbooks/chef_infrastructure/recipes/rubygems.rb
@@ -1,17 +1,6 @@
 # Manage permissions for our rubygems.
 
 rubygems do
-  # ::ALL_CHEF_GEMS.each do |name|
-  #   gem = rubygems.gem(name)
-  #   if !gem.owners.include?("john@johnkeiser.com")
-  #     if gem.owners.include?("danielsdeleo@mac.com")
-  #       puts "gem owner #{name} -a john@johnkeiser.com"
-  #     else
-  #       puts "Find an owner for #{name}: #{gem.owners.inspect}"
-  #     end
-  #   end
-  # end
-
   ::ALL_CHEF_GEMS.each do |name|
     gem name do
       owners Permissions.rubygem_owners(name)
