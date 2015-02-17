e45707b9bd1907ee263a7aee4fed9f404fd51a67
John Keiser <john@johnkeiser.com>
2015-02-17 10:06:18 -0800

    Get rid of extra newline in Chef output (fix #12)

--- lib/crazytown/chef_dsl/chef_resource_log.rb
+++ lib/crazytown/chef_dsl/chef_resource_log.rb
@@ -18,7 +18,7 @@ module Crazytown
       # When load happens, notify Chef that the resource's current state is loaded.
       def load_succeeded
         super
-        resource.events.resource_current_state_loaded(self, action, resource.current_resource)
+        resource.events.resource_current_state_loaded(resource, action, resource.current_resource)
       end
 
       # When an update succeeds, we mark the resource
@@ -26,16 +26,17 @@ module Crazytown
         super
 
         if resource.updated_by_last_action?
-          resource.events.resource_updated(self, action)
+          resource.events.resource_updated(resource, action)
         else
-          resource.events.resource_up_to_date(self, action)
+          resource.events.resource_up_to_date(resource, action)
         end
       end
 
       def action_skipped(description, update_guaranteed: true)
         super
+
         if update_guaranteed
-          resource.events.resource_update_applied(self, action, description)
+          resource.events.resource_update_applied(resource, action, description)
           resource.updated_by_last_action true
         end
       end
@@ -44,7 +45,7 @@ module Crazytown
       def action_succeeded(**args)
         description, updated = super
         if updated
-          resource.events.resource_update_applied(self, action, description)
+          resource.events.resource_update_applied(resource, action, description)
           resource.updated_by_last_action true
         end
       end
