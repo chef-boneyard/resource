342fd08f7823b691bf111c3dd4fe1c5bf8f8f7c5
Tim Smith <tsmith84@gmail.com>
2015-10-31 19:40:42 -0700

    Add maintainer and license info

--- metadata.rb
+++ metadata.rb
@@ -1,4 +1,7 @@
 name "resource"
+maintainer 'Chef Software, Inc.'
+maintainer_email 'cookbooks@chef.io'
+license 'Apache 2.0'
 description "Easier, more powerful Chef Resources"
 require_relative 'files/lib/chef_resource/version'
 version ChefResource::VERSION
